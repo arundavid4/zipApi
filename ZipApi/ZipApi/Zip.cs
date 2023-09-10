6f797a6d1643196cf06e066e491568546d1c6310
UDNBNOR\AD00574 <arun.david@dnb.no>
2023-09-10 18:52:17 +0000

    byte size modified

--- ZipApi/ZipApi/Zip.cs
+++ ZipApi/ZipApi/Zip.cs
@@ -9,10 +9,10 @@ public static class Zip
 
         using (var retur = new ZipArchive(str, ZipArchiveMode.Create))
         {
-            var buffer = new byte[0x10000];
+            var buffer = new byte[0x100000];
             for (var i = 0; i < files; i++)
             {
-                int length = stream.Read(buffer, 0, 10000);
+                int length = stream.Read(buffer, 0, 100000);
                 var name = i.ToString();
                 var entry = retur.CreateEntry(name);
                 using (var entryStream = entry.Open())
